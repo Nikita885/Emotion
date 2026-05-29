34f31a9b334475b1b2e114b76337812d2bc7b664
Nikita885 <faikqweas@mail.ru>
2026-05-30 03:13:27 +0500

    debug: add uncaughtException and unhandledRejection handlers to expose crash reason

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,6 +10,15 @@ import meEmotionColorsRouter from "./routes/meEmotionColors";
 
 dotenv.config();
 
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION:", err);
+  process.exit(1);
+});
+process.on("unhandledRejection", (reason) => {
+  console.error("UNHANDLED REJECTION:", reason);
+  process.exit(1);
+});
+
 const app = express();
 
 app.use(express.json());
